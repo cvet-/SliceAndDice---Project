84e906ebd0e70f3a3fcd00b5fae0e2e676ee9ebe
Cvetelina Mladenova <cvetelina_ml@yahoo.com>
2016-02-12 23:01:59 +0200

    slice and dice project

---
+++ WebApplication2/public/scripts/custom.js
@@ -0,0 +1,82 @@
+﻿jQuery.noConflict();
+
+jQuery(document).ready(function () {
+    var et_theme_folder = jQuery("meta[name=et_theme_folder]").attr('content'),
+		$et_top_menu = jQuery('ul#top-menu > li > ul'),
+		disableToptier = jQuery("meta[name=disableToptier]").attr('content');
+
+    jQuery('ul.nav').superfish({
+        delay: 600,
+        animation: { opacity: 'show', height: 'show' },
+        speed: 'fast',
+        autoArrows: true,
+        dropShadows: false
+
+
+    });
+
+
+    var $footer_widget = jQuery("#footer-widgets .footer-widget");
+    if ($footer_widget.length) {
+        $footer_widget.each(function (index, domEle) {
+            if ((index + 1) % 4 == 0) jQuery(domEle).addClass("last").after("<div class='clear'></div>");
+        });
+    }
+
+    et_search_bar();
+
+    function et_search_bar() {
+        var $searchform = jQuery('#social div#search-form'),
+			$searchinput = $searchform.find("input#searchinput"),
+			searchvalue = $searchinput.val();
+
+        $searchinput.focus(function () {
+            if (jQuery(this).val() === searchvalue) jQuery(this).val("");
+        }).blur(function () {
+            if (jQuery(this).val() === "") jQuery(this).val(searchvalue);
+        });
+    }
+
+    if (disableToptier == 1) jQuery("ul.nav > li > ul").prev("a").attr("href", "#");
+
+    var $comment_form = jQuery('form#commentform');
+    $comment_form.find('input:text, textarea').focus(function () {
+        if (jQuery(this).val() === jQuery(this).next('label').text()) jQuery(this).val("");
+    }).blur(function () {
+        if (jQuery(this).val() === "") jQuery(this).val(jQuery(this).next('label').text());
+    });
+
+
+
+    jQuery('.service .thumb a').hover(function () {
+        jQuery(this).find('img').stop(true, true).fadeTo('fast', 0.8).end().find('span').fadeTo('fast', 1);
+    }, function () {
+        jQuery(this).find('img').stop(true, true).fadeTo('fast', 1).end().find('span').fadeTo('fast', 0);
+    });
+
+    $multi_media_bar = jQuery('#multi-media #media-slides');
+    if ($multi_media_bar.length) {
+        $multi_media_bar.cycle({
+            fx: 'fade',
+            timeout: 0,
+            speed: 500,
+            cleartypeNoBg: true,
+            prev: 'a#left-multi-media',
+            next: 'a#right-multi-media'
+        });
+    }
+
+    $multi_media_bar.find('.thumb a').hover(function () {
+        jQuery(this).find('img').stop(true, true).fadeTo(400, 0.7).end().find('span').fadeTo(400, 1);
+        jQuery(this).parent('.thumb').find('.media-description').stop(true, true).css({ 'display': 'block', 'opacity': 0 }).animate({ opacity: 1, bottom: '53px' }, 400);
+    }, function () {
+        jQuery(this).find('img').stop(true, true).fadeTo(400, 1).end().find('span').fadeTo(400, 0);
+        jQuery(this).parent('.thumb').find('.media-description').stop(true, true).animate({ opacity: 0, bottom: '63px' }, 400, function () {
+            jQuery(this).css({ 'display': 'none' });
+        });
+    });
+
+
+});
+
+
---
+++ WebApplication2/public/scripts/flexslider.js
@@ -0,0 +1,87 @@
+﻿jQuery(window).load(function ($) {
+    var $featured = jQuery('#featured'),
+		featuredSlider = jQuery("meta[name=featuredSlider]").attr('content'),
+		featuredSpeed = jQuery("meta[name=featuredSpeed]").attr('content'),
+		$mobileNavButton = jQuery('#mobile_nav'),
+		$nav = jQuery('ul#top-menu'),
+		containerWidth = jQuery('#container').width(),
+		$cloned_nav;
+
+    if ($featured.length) {
+        sliderSettings = {
+
+            controlsContainer: '#featured #controllers_wrapper',
+            slideshow: false
+        }
+
+        if ('1' === featuredSlider) {
+            sliderSettings.slideshow = true;
+            sliderSettings.slideshowSpeed = featuredSpeed;
+        }
+
+        sliderSettings.pauseOnHover = true;
+
+        $featured.flexslider(sliderSettings);
+    }
+
+    if (!jQuery('html#ie7').length) {
+        $nav.clone().attr('id', 'mobile_menu').removeClass().appendTo($mobileNavButton);
+        $cloned_nav = $mobileNavButton.find('> ul');
+
+        $mobileNavButton.click(function () {
+            if (jQuery(this).hasClass('closed')) {
+                jQuery(this).removeClass('closed').addClass('opened');
+                $cloned_nav.slideDown(500);
+            } else {
+                jQuery(this).removeClass('opened').addClass('closed');
+                $cloned_nav.slideUp(500);
+            }
+            return false;
+        });
+
+        $mobileNavButton.find('a').click(function (event) {
+            event.stopPropagation();
+        });
+
+        jQuery(window).resize(function () {
+            if (containerWidth != jQuery('#container').width()) {
+                containerWidth = jQuery('#container').width();
+
+                et_mobile_navigation_fix();
+                et_footer_widgets_fix();
+            }
+        });
+
+        et_mobile_navigation_fix();
+        et_footer_widgets_fix();
+    }
+
+    function et_mobile_navigation_fix() {
+        var et_left;
+
+        if (containerWidth <= 480) {
+            et_left = (containerWidth - $mobileNavButton.innerWidth()) / 2;
+            if (containerWidth <= 300) {
+                et_left = et_left - 31;
+            } else {
+                et_left = et_left - 52;
+            }
+            $cloned_nav.css('left', '-' + et_left + 'px');
+        }
+    }
+
+    function et_footer_widgets_fix() {
+        var $footer_widget = jQuery("#footer-widgets .footer-widget"),
+			footer_columns_num;
+
+        footer_columns_num = containerWidth <= 768 ? 3 : 4;
+
+        if ($footer_widget.length) {
+            $footer_widget.removeClass('last').closest('#footer-widgets').find('div.clear').remove();
+
+            $footer_widget.each(function (index, domEle) {
+                if ((index + 1) % footer_columns_num == 0) jQuery(domEle).addClass("last").after("<div class='clear'></div>");
+            });
+        }
+    }
+});
